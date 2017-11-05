59cea771e2e2eaf9c817cef4e82794f3630b5565
Mike Grip <mgrip@Mikes-MacBook-Pro.local>
2017-11-05 16:27:57 -0500

    fixing port override

--- scripts/startd.js
+++ scripts/startd.js
@@ -47,9 +47,7 @@ if (process.env.NODE_ENV !== "production") {
 } else {
   config.plugins = [
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
+      "process.env.NODE_ENV": JSON.stringify("production")
     }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.DedupePlugin(),
