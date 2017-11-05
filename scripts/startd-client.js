ed5fbfd4d88b97aa84c44937d8afdbab72e6d0a8
Mike Grip <mgrip@Mikes-MacBook-Pro.local>
2017-11-04 20:46:37 -0400

    updating production build

--- scripts/startd-client.js
+++ scripts/startd-client.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../src/App.js'
-import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import { BrowserRouter } from 'react-router-dom'
-import { AppContainer } from 'react-hot-loader'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/App.js";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import { AppContainer } from "react-hot-loader";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore((state = {}, action) => state, composeEnhancers())
+const store = createStore((state = {}, action) => state, composeEnhancers());
 
 const render = Component => {
   ReactDOM.render(
@@ -19,26 +19,26 @@ const render = Component => {
         </BrowserRouter>
       </Provider>
     </AppContainer>,
-    document.getElementById('root')
-  )
-}
+    document.getElementById("root")
+  );
+};
 
-document.addEventListener('DOMContentLoaded', event => {
-  render(App)
-})
+document.addEventListener("DOMContentLoaded", event => {
+  render(App);
+});
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('../src/App.js', () => {
-    const NextApp = require('../src/App.js').default
-    render(NextApp)
-  })
+  module.hot.accept("../src/App.js", () => {
+    const NextApp = require("../src/App.js").default;
+    render(NextApp);
+  });
 }
 
-const socket = new WebSocket('ws://' + window.location.host)
+const socket = new WebSocket("wss://" + window.location.host);
 socket.onopen = () => {
-  console.log('connected to startd server')
-}
+  console.log("connected to startd server");
+};
 socket.onmessage = e => {
-  console.log(e)
-}
+  console.log(e);
+};
--- scripts/startd.js
+++ scripts/startd.js
@@ -1,47 +1,56 @@
-const webpack = require('webpack')
-const path = require('path')
-const fork = require('child_process').fork
-const express = require('express')
-const WebpackDevServer = require('webpack-dev-server')
-const formatWebpackMessages = require('webpack-format-messages')
+const webpack = require("webpack");
+const path = require("path");
+const fork = require("child_process").fork;
+const express = require("express");
+const WebpackDevServer = require("webpack-dev-server");
+const formatWebpackMessages = require("webpack-format-messages");
 
-const publicPath = path.resolve(process.cwd(), 'public')
-const scriptsPath = path.resolve(process.cwd(), 'scripts')
-const serverEntry = 'startd-server.js'
-const clientEntry = 'startd-client.js'
-const serverBundle = 'startd-server.bundle.js'
-const clientBundle = 'startd.bundle.js'
-const port = process.env.PORT || 3000
-const hotModuleReplacementPort = 8000
+const publicPath = path.resolve(process.cwd(), "public");
+const scriptsPath = path.resolve(process.cwd(), "scripts");
+const serverEntry = "startd-server.js";
+const clientEntry = "startd-client.js";
+const serverBundle = "startd-server.bundle.js";
+const clientBundle = "startd.bundle.js";
+const port = process.env.PORT || 3000;
+const hotModuleReplacementPort = 8000;
 
-let server = null
+let server = null;
 const refresh = () => {
-  if (server) server.kill()
-  server = fork(path.resolve(publicPath, serverBundle), { stdio: 'inherit' })
-  console.log('Restarted the server')
-}
+  if (server) server.kill();
+  server = fork(path.resolve(publicPath, serverBundle), { stdio: "inherit" });
+  console.log("Restarted the server");
+};
 
 const config = {
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
           babelrc: false,
           presets: [
-            'babel-preset-es2015',
-            'babel-preset-stage-1',
-            'babel-preset-react'
+            "babel-preset-es2015",
+            "babel-preset-stage-1",
+            "babel-preset-react"
           ]
         }
       }
     ]
   },
-  devtool: 'eval'
-}
-if (process.env.NODE_ENV !== 'production') {
-  config.module.rules[0].options.plugins = ['react-hot-loader/babel']
+  devtool: "eval"
+};
+if (process.env.NODE_ENV !== "production") {
+  config.module.rules[0].options.plugins = ["react-hot-loader/babel"];
+} else {
+  config.plugins = [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ];
 }
 
 const serverConfig = Object.assign({}, config, {
@@ -50,74 +59,73 @@ const serverConfig = Object.assign({}, config, {
     path: publicPath,
     filename: serverBundle
   },
-  target: 'node'
-})
+  target: "node"
+});
 const clientConfig = Object.assign({}, config, {
   entry: [path.resolve(scriptsPath, clientEntry)],
   output: {
     path: publicPath,
     filename: clientBundle
   }
-})
-debugger
-if (process.env.NODE_ENV !== 'production') {
-  debugger
+});
+if (process.env.NODE_ENV !== "production") {
   clientConfig.entry.unshift(
-    'react-hot-loader/patch',
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:' + hotModuleReplacementPort
-  )
+    "react-hot-loader/patch",
+    "webpack/hot/dev-server",
+    "webpack-dev-server/client?http://localhost:" + hotModuleReplacementPort
+  );
   clientConfig.plugins = [
+    ...clientConfig.plugins,
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin()
-  ]
+  ];
 }
 
 const outputMessages = stats => {
-  const messages = formatWebpackMessages(stats)
+  const messages = formatWebpackMessages(stats);
 
   if (!messages.errors.length && !messages.warnings.length) {
-    console.log('Compiled successfully!')
+    console.log("Compiled successfully!");
   }
 
   if (messages.errors.length) {
-    console.log('Failed to compile.')
-    messages.errors.forEach(e => console.log(e))
-    return
+    console.log("Failed to compile.");
+    messages.errors.forEach(e => console.log(e));
+    return;
   }
 
   if (messages.warnings.length) {
-    console.log('Compiled with warnings.')
-    messages.warnings.forEach(w => console.log(w))
+    console.log("Compiled with warnings.");
+    messages.warnings.forEach(w => console.log(w));
   }
-}
+};
 
 const createCompiler = config => {
-  const compiler = webpack(config)
-  compiler.plugin('invalid', () => console.log('Compiling...'))
-  compiler.plugin('done', stats => outputMessages(stats))
-  return compiler
-}
+  const compiler = webpack(config);
+  compiler.plugin("invalid", () => console.log("Compiling..."));
+  compiler.plugin("done", stats => outputMessages(stats));
+  return compiler;
+};
 
-if (process.env.NODE_ENV === 'production') {
-  webpack([serverConfig, clientConfig]).run(err => {
-    refresh()
-  })
+if (process.env.NODE_ENV === "production") {
+  webpack([serverConfig, clientConfig]).run((err, stats) => {
+    refresh();
+  });
 } else {
   const server = new WebpackDevServer(createCompiler(clientConfig), {
     contentBase: publicPath,
-    publicPath: '/',
+    publicPath: "/",
     hot: true,
     inline: true,
     proxy: {
-      '/{!(startd.bundle.js|sockjs-node|hot-update)/**,!(startd.bundle.js|sockjs-node|hot-update),}': {
-        target: 'http://localhost:' + port + '/',
+      "/{!(startd.bundle.js|sockjs-node|hot-update)/**,!(startd.bundle.js|sockjs-node|hot-update),}": {
+        target: "http://localhost:" + port + "/",
         ws: true,
-        logLevel: 'debug'
+        logLevel: "debug"
       }
     }
-  }).listen(hotModuleReplacementPort)
+  }).listen(hotModuleReplacementPort);
   createCompiler(serverConfig).watch({}, err => {
-    refresh()
-  })
+    refresh();
+  });
 }
