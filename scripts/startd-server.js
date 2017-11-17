044e78f830f91f48a6a2355d1e77180bd74141ac
Mike Grip <mgrip@Mikes-MacBook-Pro.local>
2017-11-16 21:52:16 -0500

    fix bundle route

--- scripts/startd-server.js
+++ scripts/startd-server.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import express from 'express'
-import { renderToString } from 'react-dom/server'
-import App from '../src/App.js'
-import { StaticRouter } from 'react-router-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import WebSocket from 'ws'
+import React from "react";
+import express from "express";
+import { renderToString } from "react-dom/server";
+import App from "../src/App.js";
+import { StaticRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import WebSocket from "ws";
 
-const store = createStore((state = {}) => state)
+const store = createStore((state = {}) => state);
 
 const server = express()
-  .use(express.static('public'))
-  .get('*', (req, res) => {
+  .use(express.static("public"))
+  .get("*", (req, res) => {
     res.status(200).send(`<!doctype html><html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script type="text/javascript" src="startd.bundle.js"></script>
+        <script type="text/javascript" src="/startd.bundle.js"></script>
       </head>
       <body>
         <div id="root">
@@ -29,15 +29,15 @@ const server = express()
           )}
         </div>
       </body>
-    </html>`)
+    </html>`);
   })
-  .listen(process.env.PORT || 3000)
+  .listen(process.env.PORT || 3000);
 
-const ws = new WebSocket.Server({ server })
+const ws = new WebSocket.Server({ server });
 
-ws.on('connection', socket => {
-  console.log('hi client connected')
-  socket.on('disconnect', () => {
-    console.log('client disconnect')
-  })
-})
+ws.on("connection", socket => {
+  console.log("hi client connected");
+  socket.on("disconnect", () => {
+    console.log("client disconnect");
+  });
+});
