f62c3abf2a0a62064b9424424c5935e0f6488d3a
Mike Grip <mgrip@wayfair.com>
2018-05-02 21:34:46 -0400

    using default input argument for path

--- packages/startd-server/lib/index.js
+++ packages/startd-server/lib/index.js
@@ -3,6 +3,8 @@
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _webpack = require("webpack");
 
 var _webpack2 = _interopRequireDefault(_webpack);
@@ -19,9 +21,9 @@ var _chalk = require("chalk");
 
 var _chalk2 = _interopRequireDefault(_chalk);
 
-var _minimist = require("minimist");
+var _minimist2 = require("minimist");
 
-var _minimist2 = _interopRequireDefault(_minimist);
+var _minimist3 = _interopRequireDefault(_minimist2);
 
 var _findUp = require("find-up");
 
@@ -45,14 +47,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var argv = (0, _minimist2.default)(process.argv.slice(2));
+var _minimist = (0, _minimist3.default)(process.argv.slice(2)),
+    _minimist$_ = _slicedToArray(_minimist._, 1),
+    inputPath = _minimist$_[0];
 
-if (typeof argv.path !== "string") {
-  _logger2.default.error('You must provide a valid "--path" argument, as a path to your root App component.');
+if (typeof inputPath !== "string") {
+  _logger2.default.error("You must provide a valid path to your root App component");
   process.exit(1);
 }
 // $FlowFixMe - for some reason flow isn't picking up the process.exit above
-var appPath = _path2.default.resolve(process.cwd(), argv.path);
+var appPath = _path2.default.resolve(process.cwd(), inputPath);
 if (!_fs2.default.existsSync(appPath)) {
   _logger2.default.error(appPath + " is not a valid filepath \uD83D\uDE3F");
   process.exit(1);
@@ -138,7 +142,7 @@ var appConfig = _webpackConfig2.default.map(function (singleConfig) {
     return;
   }
 
-  reactHotLoader.register(argv, "argv", "src/index.js");
+  reactHotLoader.register(inputPath, "inputPath", "src/index.js");
   reactHotLoader.register(appPath, "appPath", "src/index.js");
   reactHotLoader.register(appConfig, "appConfig", "src/index.js");
   leaveModule(module);
