2c5cc51853c0bbf6a4d929387f2078ca4b6d6f01
Mike Grip <michael.d.grip@icloud.com>
2018-12-04 17:31:55 -0500

    react hot loader

--- src/reactHotLoaderConfig.js
+++ src/reactHotLoaderConfig.js
@@ -1,6 +1,8 @@
-import { setConfig } from "react-hot-loader";
+import { setConfig, cold } from "react-hot-loader";
 
 setConfig({
-  ignoreSFC: true, // RHL will be __completely__ disabled for SFC
-  pureRender: true // RHL will not change render method
+  onComponentCreate: (type, name) =>
+    (String(type).indexOf("useState") > 0 ||
+      String(type).indexOf("useEffect") > 0) &&
+    cold(type)
 });
