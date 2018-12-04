c858a5cc92b4fa9c3e82d1db2d6eaaa52106e84d
Mike Grip <michael.d.grip@icloud.com>
2018-12-04 15:13:23 -0500

    support react hooks

--- src/client.js
+++ src/client.js
@@ -1,9 +1,13 @@
 // @flow strict
 
 import React from "react";
-import { hot } from "react-hot-loader";
+import { hot, setConfig } from "react-hot-loader";
 import { hydrate } from "react-dom";
 
+setConfig({
+  ignoreSFC: true, // RHL will be __completely__ disabled for SFC
+  pureRender: true // RHL will not change render method
+});
 const App = require(APP_PATH).default;
 const HotApp = hot(module)(App);
 
